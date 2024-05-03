c206df832f3b93b0a16a0c06b664314669d6bfa1
jimmoers1 <150346272+jimmoers1@users.noreply.github.com>
2024-05-03 15:26:30 +0200

    Add files via upload

--- streamlit_deploy_test.py
+++ streamlit_deploy_test.py
@@ -1,5 +1,20 @@
+import datetime
+
 import streamlit as st
 
-st.title("test")
-st.markdown("test")
-st.write("hello world")
+st.title("Deliverable order predictor")
+
+# Allows you to pick a date in 2024
+min_value = datetime.date(2024, 1, 1)
+max_value = datetime.date(2024, 12, 31)
+date = st.date_input("What date would you like to predict?", datetime.date(2024, 1, 1), min_value, max_value)
+st.write("You picked", date)
+
+rain = st.selectbox("is it going to rain on this day?", ["no", "yes"])
+
+if rain == "yes":
+    rain_amount = st.text_input("How much mm is it going to rain on this day?")
+    if rain_amount:
+        st.write(rain_amount, "mm")
+    else:
+        st.write("")
