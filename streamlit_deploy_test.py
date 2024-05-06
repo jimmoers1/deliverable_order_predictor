e31ce9a0eb7c6ce480a06970694f219eeeaaf58d
jimmoers1 <150346272+jimmoers1@users.noreply.github.com>
2024-05-06 10:23:14 +0200

    Add files via upload

--- streamlit_deploy_test.py
+++ streamlit_deploy_test.py
@@ -8,13 +8,19 @@ st.title("Deliverable order predictor")
 min_value = datetime.date(2024, 1, 1)
 max_value = datetime.date(2024, 12, 31)
 date = st.date_input("What date would you like to predict?", datetime.date(2024, 1, 1), min_value, max_value)
-st.write("You picked", date)
+st.write("You picked", str(date))
 
-rain = st.selectbox("is it going to rain on this day?", ["no", "yes"])
+rain = st.selectbox("Is it going to rain on this day?", ["No", "Yes", "I don't know"])
 
-if rain == "yes":
+# Creates an extra option that allows you to provide the amount of rain
+if rain == "Yes":
     rain_amount = st.text_input("How much mm is it going to rain on this day?")
     if rain_amount:
-        st.write(rain_amount, "mm")
+        try:  # try if it is possible to convert the rain amount to integer
+            int(rain_amount)
+            st.write(rain_amount, "mm")  # print the number as a string
+            rain_amount = int(rain_amount)  # change the number to an int
+        except ValueError:
+            st.error("Please provide a whole number")
     else:
-        st.write("")
+        st.write("")  # doesn't display anything when no number is given
